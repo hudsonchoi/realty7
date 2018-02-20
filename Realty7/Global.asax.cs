e6927c0034916fa0ac530082dcef206118b567cf
WIN-SN1HFN8BKRU\Chodae <hudsonchoi@gmail.com>
2018-02-19 20:44:36 -0500

    v1.0.0

--- Realty7/Global.asax.cs
+++ Realty7/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
+using System.Reflection;
 using System.Text;
 using System.Web;
 using System.Web.Http;
@@ -19,11 +20,22 @@ namespace Realty7
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
+            GlobalFilters.Filters.Add(new MyPropertyActionFilter(), 0);
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public class MyPropertyActionFilter : ActionFilterAttribute
+        {
+            public override void OnResultExecuting(ResultExecutingContext filterContext)
+            {
+                Assembly asm = Assembly.GetExecutingAssembly();
+                string[] parts = asm.FullName.Split(',');
+                var ver = parts[1].Replace("Version=", "v");
+                filterContext.Controller.ViewBag.Version = ver.Substring(0, ver.LastIndexOf(".")); ;
+            }
         }
 
         void Application_Error(Object sender, EventArgs e) 
